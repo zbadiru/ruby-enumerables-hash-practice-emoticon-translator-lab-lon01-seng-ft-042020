29c1f2f72dfc6465315a4479c3487164cd21472f
Zakari Badiru <zbadiru@live.co.uk>
2020-04-08 10:22:59 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -17,6 +17,8 @@ def get_japanese_emoticon(file_path, emoticons)
   library = load_library(file_path)
   emoticons = library.keys.find do |key|
     library[key][:english] == emoticons
+  end
+  
 end
 
 def get_english_meaning
