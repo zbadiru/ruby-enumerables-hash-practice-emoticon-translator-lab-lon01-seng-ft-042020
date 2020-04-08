324aab6d5b2d763012295edb311c3e3134c4ad04
Zakari Badiru <zbadiru@live.co.uk>
2020-04-08 10:10:09 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,16 @@
-# require modules here
+require "yaml"
 
-def load_library
-  # code goes here
+
+def load_library(file_path)
+  emoticons = YAML.load_file(file_path)
+  
+  new_hash = { }
+  emoticons.each do |key, value|
+    new_hash[key] = { }
+    new_hash[key][:english] = value[0]
+    new_hash[key][:japanese] = value[1]
+  end
+  new_hash
 end
 
 def get_japanese_emoticon
